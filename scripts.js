d20933d42f639c17ad0b52a4ef0d9cab837258e2
atrem <Bobcopy555@gmail.com>
2022-04-06 18:03:39 +0500

    start js validating

--- scripts.js
+++ scripts.js
@@ -2,21 +2,28 @@ $(document).ready(function () {
 
     $('form').on('submit', function () {
 
-        var name = $('form name');
-        var phone = $('form phone');
-        var email = $('form email');
+        let name = $('form name');
+        let phone = $('form phone');
+        let email = $('form email');
+
+        // JS validation
+        const regexRuName = /[А-Яа-я-]+/;
+        const regexPhone = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
+        const regexEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        // TODO: дописать js валидацию!
+
         var data = {
             name: name.val(),
             phone: phone.val(),
             email: email.val()
         };
 
+
         $.ajax({
             type: 'POST',
             url: 'https://60376bfd5435040017722533.mockapi.io/form',
             data: data,
             success: function (data) {
-                //do something with the data via front-end framework
                 alert('Succes!');
                 location.reload();
             },
